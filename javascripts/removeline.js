6ecabd378ba1ad8824135a291a464ef68efee59d
David Myers <davidmyers9000@gmail.com>
2014-08-04 10:33:40 -0400

    Automated commit at 2014-08-04 14:33:40 UTC by middleman-deploy 0.2.4

--- javascripts/removeline.js
+++ javascripts/removeline.js
@@ -1,7 +1,1 @@
-// JavaScript Document
-
-theObjects = document.getElementsByTagName("object");
-for (var i = 0; i < theObjects.length; i++) {
-theObjects[i].outerHTML = theObjects[i].outerHTML;
-} 
-;
+theObjects=document.getElementsByTagName("object");for(var i=0;i<theObjects.length;i++)theObjects[i].outerHTML=theObjects[i].outerHTML;
