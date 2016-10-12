afcca15ea8338b3673b4acb0a6aeca67722dd01f
David Myers <davidmyers9000@gmail.com>
2016-10-12 13:56:50 -0400

    created layout for vertical timeline

--- config.rb
+++ config.rb
@@ -195,10 +195,10 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  activate :relative_assets
+  # activate :relative_assets
 
   # Or use a different image path
-  set :http_prefix, "/labs/newsletter"
+  set :http_prefix, "/media/newsletter"
 
   # activate :gzip
   # activate :minify_html
