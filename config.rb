dc02c47fea77d94d346c0260b07b1c5f9f8497f7
Dave Myers <drm68@drexel.edu>
2019-05-01 16:05:56 -0400

    added senior showcase official invitation

--- config.rb
+++ config.rb
@@ -83,75 +83,75 @@ helpers do
       # puts details
 
       items << {:title       => details[1],
-                :description => fix_relative_img(details[3]),
-                :link        => details[5],
-                :pubDate     => parse_date(details[7]),
-                :guid        => details[9]}
+        :description => fix_relative_img(details[3]),
+        :link        => details[5],
+        :pubDate     => parse_date(details[7]),
+        :guid        => details[9]}
+      end
+      return items
     end
-    return items
-  end
 
-  def bme_path
-    "http://www.biomed.drexel.edu/media/newsletter/"
-  end
+    def bme_path
+      "http://www.biomed.drexel.edu/media/newsletter/"
+    end
 
-  def newsletter_base_href
-    "http://biomed.drexel.edu/media/newsletter/"
-  end
+    def newsletter_base_href
+      "http://biomed.drexel.edu/media/newsletter/"
+    end
 
-  def format_date(string)
-    Date.strptime(string, '%m-%d-%y').strftime("%B %d, %Y")
-  end
+    def format_date(string)
+      Date.strptime(string, '%m-%d-%y').strftime("%B %d, %Y")
+    end
 
-  def data_file
-    current_page.data.data_file
-  end
+    def data_file
+      current_page.data.data_file
+    end
 
-  def quarter_term
-    eval("data."+"#{data_file}.term")
-  end
+    def quarter_term
+      eval("data."+"#{data_file}.term")
+    end
 
-  def read_in_browser
-    newsletter_base_href + "#{quarter_term.downcase.gsub(" ", "-")}.html"
-  end
+    def read_in_browser
+      newsletter_base_href + "#{quarter_term.downcase.gsub(" ", "-")}.html"
+    end
 
-  def spotlight
-    eval("data."+"#{data_file}.spotlight")
-  end
+    def spotlight
+      eval("data."+"#{data_file}.spotlight")
+    end
 
-  def students
-    eval("data."+"#{data_file}.students")
-  end
+    def students
+      eval("data."+"#{data_file}.students")
+    end
 
-  def research
-    eval("data."+"#{data_file}.research")
-  end
+    def research
+      eval("data."+"#{data_file}.research")
+    end
 
-  def faculty
-    eval("data."+"#{data_file}.faculty")
-  end
+    def faculty
+      eval("data."+"#{data_file}.faculty")
+    end
 
-  def faculty_even
-    faculty.select.each_with_index{ |_, i| i.even? }
-  end
+    def faculty_even
+      faculty.select.each_with_index{ |_, i| i.even? }
+    end
 
-  def faculty_odd
-    faculty.select.each_with_index{ |_, i| i.odd? }
-  end
+    def faculty_odd
+      faculty.select.each_with_index{ |_, i| i.odd? }
+    end
 
-  def alumni
-    eval("data."+"#{data_file}.alumni")
-  end
+    def alumni
+      eval("data."+"#{data_file}.alumni")
+    end
 
-  def utm_source
-    current_page.data.utm_source ||= current_page.data.source
-  end
+    def utm_source
+      current_page.data.utm_source ||= current_page.data.source
+    end
 
-  def utm_medium
-    "HTML_Email"
-  end
+    def utm_medium
+      "HTML_Email"
+    end
 
-  def home_link
+    def home_link
       html = '<a target="_blank" href="http://drexel.edu/biomed'
       if environment == :development
         html += "#development"
@@ -163,92 +163,94 @@ helpers do
         html += gua
         html += '">'
       end
-  end
-
-  def end_link
-    "</a>"
-  end
-
-  def a_href(section, index=888)
-    if index == 888
-      html = '<a target="_blank" href="'
-      html += eval("#{section}.url")
-      html += gua
-      html += '">'
-    elsif index == 777
-
-    else
-      html = '<a target="_blank" href="'
-      html += eval("#{section}"+"["+"#{index}"+"].url")
-      html += gua
-      html += '">'
     end
-  end
 
-  def link_me (title, link)
+    def end_link
+      "</a>"
+    end
+
+    def a_href(section, index=888)
+      if index == 888
+        html = '<a target="_blank" href="'
+        html += eval("#{section}.url")
+        html += gua
+        html += '">'
+      else
+        html = '<a target="_blank" href="'
+        html += eval("#{section}"+"["+"#{index}"+"].url")
+        html += gua
+        html += '">'
+      end
+    end
+
+    def link_me (title, link)
       html = '<a target="_blank" href="'
       html += link
       html += '">'
       html += title
       html += '</a>'
-  end
+    end
 
-  # def utm_term
-    # depends on where in the newsletter it is
-    # i.e. alumni, spotlight, students, faculty
-  # end
+    # def utm_term
+      # depends on where in the newsletter it is
+      # i.e. alumni, spotlight, students, faculty
+    # end
 
-  def utm_campaign
-    # grab info directly from YAML file
-    current_page.data.utm_campaign ||= current_page.data.campaign
-  end
-
-  def utm_term
-    if environment == :development
-      # "This is in development"
-    else
+    def utm_campaign
       # grab info directly from YAML file
-      current_page.data.term ||= current_page.data.term
+      current_page.data.utm_campaign ||= current_page.data.campaign
+    end
+
+    def utm_term
+      if environment == :development
+        # "This is in development"
+      else
+        # grab info directly from YAML file
+        current_page.data.term ||= current_page.data.term
+      end
+    end
+
+    def utm_dp
+      current_page.data.utm_dp ||= ""
+    end
+
+    def utm_dt
+      current_page.data.utm_dt ||= ""
+    end
+
+    def utm_dl
+      current_page.data.utm_dl ||= ""
+    end
+
+    def ga
+      if environment == :development
+        # "This is in development"
+      else
+        "?utm_source=#{utm_source}&amp;utm_medium=#{utm_medium}&amp;utm_campaign=#{utm_campaign}"
+      end
+    end
+
+    def gua
+      if environment == :development
+        '#'
+      else
+        "?utm_source=#{utm_source}&utm_medium=#{utm_medium}&utm_campaign=#{utm_campaign}"
+      end
+    end
+
+    def tracking_id
+      "UA-87264639-1"
+    end
+
+    def track_opens_url
+      if environment == :development
+        tracking_link = "https://placehold.it/5x5"
+      else
+        tracking_link = "https://google-analytics.com/collect?v=1&tid=#{tracking_id}&cid=333&t=event&ec=email&ea=open&cn=#{utm_campaign}&cs=#{utm_source}&cm=#{utm_medium}&dt=#{utm_dt}&dp=#{utm_dp}&dl=#{utm_dl}"
+      end
+      "<img src=\"#{tracking_link}\" />"
     end
   end
-
-  def utm_dp
-    current_page.data.utm_dp ||= ""
-  end
-
-  def utm_dt
-    current_page.data.utm_dt ||= ""
-  end
-
-  def utm_dl
-    current_page.data.utm_dl ||= ""
-  end
-
-  def ga
-    if environment == :development
-      # "This is in development"
-    else
-      "?utm_source=#{utm_source}&amp;utm_medium=#{utm_medium}&amp;utm_campaign=#{utm_campaign}"
-    end
-  end
-
-  def gua
-    "?utm_source=#{utm_source}&utm_medium=#{utm_medium}&utm_campaign=#{utm_campaign}"
-  end
-
-  def tracking_id
-    "UA-87264639-1"
-  end
-
-  def track_opens_url
-    if environment == :development
-      tracking_link = "https://placehold.it/5x5"
-    else
-      tracking_link = "https://google-analytics.com/collect?v=1&tid=#{tracking_id}&cid=333&t=event&ec=email&ea=open&cn=#{utm_campaign}&cs=#{utm_source}&cm=#{utm_medium}&dt=#{utm_dt}&dp=#{utm_dp}&dl=#{utm_dl}"
-    end
-    "<img src=\"#{tracking_link}\" />"
-  end
-end
 
 
 # Automatic image dimensions on image_tag helper
